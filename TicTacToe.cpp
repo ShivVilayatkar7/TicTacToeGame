da6110c26b2e3560ec83d731ddd11f2f0338de0e
shiv vilayatkar <138550378+ShivVilayatkar7@users.noreply.github.com>
2023-07-08 22:01:57 -0900

    Update and rename README.md to TicTacToe.cpp

---
+++ TicTacToe.cpp
@@ -0,0 +1,322 @@
+# TicTacToeGame
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+//structure array
+struct block{
+	string block[9];
+	string player1, player2;
+};
+
+block b;
+
+//player one's input
+void player1()
+{
+	back:
+		
+	int in;
+	
+	cout << "\nPlayer1" << "\n";
+	cout << "\nEnter Number from 0 to 8 :: ";
+	cin >> in;
+	
+	cout << " 1 | 2 | 3 " << "\n";
+	cout << "--- --- ---" << "\n";
+	cout << " 4 | 5 | 6 " << "\n";
+	cout << "--- --- ---" << "\n";
+	cout << " 7 | 8 | 9 " << "\n";
+	
+	cout << "\n\nNote : Remember the Above Structure then Enter Numbers. \n\n";
+	
+	if(in == 1)
+	{
+		b.block[1]={"X"};
+	}
+	else if(in == 2)
+	{
+		b.block[2]={"X"};
+	}
+	else if(in == 3)
+	{
+		b.block[3]={"X"};
+	}
+	else if(in == 4)
+	{
+		b.block[4]={"X"};
+	}
+	else if(in == 5)
+	{
+		b.block[5]={"X"};
+	}
+	else if(in == 6)
+	{
+		b.block[6]={"X"};
+	}
+	else if(in == 7)
+	{
+		b.block[7]={"X"};
+	}
+	else if(in == 8)
+	{
+		b.block[8]={"X"};
+	}
+	else if(in == 9)
+	{
+		b.block[9]={"X"};
+	}
+	else
+	{
+		cout << "Invalid input !" << "\n\n" ;
+		goto back;
+	}
+}
+
+//player two's input
+void player2()
+{
+	backk:
+		
+	int iin;
+	
+	cout << "\nPlayer2" << "\n";
+	cout << "\nEnter Number from 0 to 8 :: ";
+	cin >> iin;
+	
+	cout << " 1 | 2 | 3 " << "\n";
+	cout << "--- --- ---" << "\n";
+	cout << " 4 | 5 | 6 " << "\n";
+	cout << "--- --- ---" << "\n";
+	cout << " 7 | 8 | 9 " << "\n\n";
+	
+	cout << "\n\nNote : Remember the Above Structure then Enter Numbers. \n\n";
+	
+	if(iin == 1)
+	{
+		b.block[1]={"O"} ; 
+	}
+	else if(iin == 2)
+	{
+		b.block[2]={"O"};
+	}
+	else if(iin == 3)
+	{
+		b.block[3]={"O"};
+	}
+	else if(iin == 4)
+	{
+		b.block[4]={"O"};
+	}
+	else if(iin == 5)
+	{
+		b.block[5]={"O"};
+	}
+	else if(iin ==6)
+	{
+		b.block[6]={"O"};
+	}
+	else if(iin == 7)
+	{
+		b.block[7]={"O"};
+	}
+	else if(iin == 8)
+	{
+		b.block[8]={"O"};
+	}
+	else if(iin == 9)
+	{
+		b.block[9]={"O"};
+	}
+	else
+	{
+		cout << "Invalid input !" << "\n\n" ;
+		goto backk;
+	}
+}
+
+//function to print direct output
+void output()
+{
+	cout << "output : \n\n" ;
+	
+	//show output after every input
+	cout << b.block[1] << " | " << b.block[2] << " | " << b.block[3] << "\n" ; //1st row
+	cout << "---------" << "\n" ;
+	cout << b.block[4] << " | " << b.block[5] << " | " << b.block[6] << "\n" ; // 2nd row
+	cout << "---------" << "\n" ;
+	cout << b.block[7] << " | " << b.block[8] << " | " << b.block[9] << "\n" ; //3rd row
+}
+
+//condition to check the row, column and cross are same or note
+void check()
+{ 
+	if(b.block[1] == "X" && b.block[2] == "X" && b.block[3] == "X") //checking 1st row for player 1
+	{
+		cout << "Player 1 Wins !! " << "\n" ;
+	}
+	else if(b.block[1] == "O" && b.block[2] == "O" && b.block[3] == "O") //checking 1st row for player 2
+	{
+		cout << "Player 2 Wins !! " << "\n" ;
+	}
+	else if(b.block[4] == "X" && b.block[5] == "X" && b.block[6] == "X") //checking 2nd row for player 1
+	{
+		cout << "Player 1 Wins !! " << "\n" ;
+	}
+	else if(b.block[4] == "O" && b.block[5] == "O" && b.block[6] == "O") //checking 2nd row for player 2
+	{
+		cout << "Player 2 Wins !! " << "\n" ;
+	}
+	else if(b.block[7] == "X" && b.block[8] == "X" && b.block[9] == "X") // checking 3rd row for player 1
+	{
+		cout << "Player 1 Wins !! " << "\n" ;
+	}
+	else if(b.block[7] == "O" && b.block[8] == "O" && b.block[9] == "O") // checking 3rd row for player 2
+	{
+		cout << "Player 2 Wins !! " << "\n" ;
+	}
+	else if(b.block[1] == "X" && b.block[5] == "X" && b.block[9] == "X") //cheching cross line for player 1
+	{
+		cout << "Player 1 Wins !! " << "\n" ;
+	}
+	else if(b.block[1] == "O" && b.block[5] == "O" && b.block[9] == "O") // checking cross line for player 2
+	{
+		cout << "Player 2 Wins !! " << "\n" ;
+	}
+	else if(b.block[3] == "X" && b.block[5] == "X" && b.block[7] == "X")  //cheching cross line for player 1
+	{
+		cout << "Player 1 Wins !! " << "\n" ;
+	}
+	else if(b.block[3] == "O" && b.block[5] == "O" && b.block[7] == "O") // checking cross line for player 2
+	{
+		cout << "Player 2 Wins !! " << "\n" ;
+	}
+	else
+	{
+		cout << "\n\n Match Draw, No One Won The Match " << "\n" ;
+	}
+}
+
+// checking input while palying 
+int checking()
+{ 
+	if(b.block[1] == "X" && b.block[2] == "X" && b.block[3] == "X") //checking 1st row for player 1
+	{
+		return 1;
+	}
+	else if(b.block[1] == "O" && b.block[2] == "O" && b.block[3] == "O") //checking 1st row for player 2
+	{
+		return 2;
+	}
+	else if(b.block[4] == "X" && b.block[5] == "X" && b.block[6] == "X") //checking 2nd row for player 1
+	{
+		return 1;
+	}
+	else if(b.block[4] == "O" && b.block[5] == "O" && b.block[6] == "O") //checking 2nd row for player 2
+	{
+		return 2;
+	}
+	else if(b.block[7] == "X" && b.block[8] == "X" && b.block[9] == "X") // checking 3rd row for player 1
+	{
+		return 1;
+	}
+	else if(b.block[7] == "O" && b.block[8] == "O" && b.block[9] == "O") // checking 3rd row for player 2
+	{
+		return 2;
+	}
+	else if(b.block[1] == "X" && b.block[5] == "X" && b.block[9] == "X") //cheching cross line for player 1
+	{
+		return 1;
+	}
+	else if(b.block[1] == "O" && b.block[5] == "O" && b.block[9] == "O") // checking cross line for player 2
+	{
+		return 2;
+	}
+	else if(b.block[3] == "X" && b.block[5] == "X" && b.block[7] == "X")  //cheching cross line for player 1
+	{
+		return 1;
+	}
+	else if(b.block[3] == "O" && b.block[5] == "O" && b.block[7] == "O") // checking cross line for player 2
+	{
+		return 2;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+//main function
+int main()
+{
+	cout << "\t\t\t*************************" << "\n" ;
+	cout << "\t\t\t|     TIC-TAC-TOE       |" << "\n" ;
+	cout << "\t\t\t*************************" << "\n\n" ;
+	
+	cout << "Stucture Of Tic-Tac-Toe \n\n" ;	
+	
+	cout << " 1 | 2 | 3 " << "\n";
+	cout << "-----------" << "\n";
+	cout << " 4 | 5 | 6 " << "\n";
+	cout << "-----------" << "\n";
+	cout << " 7 | 8 | 9 " << "\n\n";
+	
+	cout << "X For Player 1. " << "\n\n";
+	
+	cout << "O For Player 2. " << "\n\n";  
+	
+	player1();
+	output();
+	
+	player2();
+	output();
+	
+	player1();
+	output();
+	
+	player2();
+	output();
+	
+	player1();
+	output();
+	if(checking() == 1)
+	{
+		cout << "Player 1 Win !" << "\n";
+		goto result;
+	}
+	
+	player2();
+	output();
+	if(checking() == 2)
+	{
+		cout << "Player 2 Win !" << "\n";
+		goto result;
+	}	
+	
+	player1();
+	output();
+	if(checking() == 1)
+	{
+		cout << "Player 1 Win !" << "\n";
+		goto result;
+	}
+	
+	player2();
+	output();
+	if(checking() == 2)
+	{
+		cout << "Player 2 Win !" << "\n";
+		goto result;
+	}
+	
+	player1();
+	output();
+	
+	result:
+		
+	check();
+	
+	return 0;
+	
+}
